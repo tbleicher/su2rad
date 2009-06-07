d72a4152ff6a58703e943e6b123bd434870256ca
tbleicher <tbleicher@ae7bd4c8-e53d-0410-bc55-1557d9083968>
2009-06-07 19:37:32 +0000

    Fixed:    typos in filesystemproxy.rb. New:      root/drives for file tree

--- branches/dev_tb/su2rad/su2radlib/filesystemproxy.rb
+++ branches/dev_tb/su2rad/su2radlib/filesystemproxy.rb
@@ -3,6 +3,11 @@ module FileSystemProxy
     def FileSystemProxy.listDirectoryTree(path)
         abspath = File.expand_path(path)
         idPath = abspath
+        while not File.exists?(abspath)
+	    puts "DEBUG: dir '#{abspath}' not a directory! - using parent"
+	    abspath = File.dirname(abspath)
+            idPath = abspath
+        end
         if File.file?(abspath)
             abspath = File.dirname(abspath)
         end
@@ -10,7 +15,7 @@ module FileSystemProxy
         children = FileSystemProxy.listDirectory(abspath,idPath)
         parent = File.basename(abspath)
         abspath = File.dirname(abspath)
-        while abspath != parent
+        while abspath
             dirs = FileSystemProxy.listDirectory(abspath,idPath)
             dirs.each { |d|
                 if d['name'] == parent
@@ -18,12 +23,49 @@ module FileSystemProxy
                 end
             }
             children = dirs
-            parent = File.basename(abspath)
-            abspath = File.dirname(abspath)
+	    if File.dirname(abspath) == abspath
+		puts "DEBUG: tree starts at '#{abspath}'"
+		children = FileSystemProxy.addRoot(abspath, children)
+        	return children
+	    else
+	        abspath = File.dirname(abspath)
+                parent = File.basename(abspath)
+	    end
         end
         return children
     end
     
+    def FileSystemProxy.addRoot(abspath, children)
+        if abspath == '/'
+	    root = {'name' => '/', 
+		    'type' => File.ftype('/'),
+		    'path' => '/',
+		    'ext'  => 'ext_drive',
+		    'access' => FileTest.readable?(drive),
+	            'children'=> children}
+	    return [root]
+	else
+	    drives = []
+	    letters = ('a'..'z').to_a
+	    letters.each { |l|
+		drive = "%s:/" % l
+	        if File.exists?(drive)
+		    puts "TEST: found drive '#{drive}'"
+		    entry = {'name' => drive, 
+			     'type' => File.ftype(drive),
+			     'path' => drive,
+			     'ext'  => 'ext_drive',
+			     'access' => FileTest.readable?(drive)}
+		    if drive == abspath
+			entry['children'] = children
+		    end
+		    drives.push(entry)
+		end
+	    }
+	    return drives
+	end
+    end
+
     def FileSystemProxy.printDirectoryTree(entries, indent="")
         entries.each { |e|
             printf "DEBUG: #{indent}#{e['name']}\n"
@@ -39,12 +81,9 @@ module FileSystemProxy
             puts "DEBUG: listing directory '#{abspath}'"
             puts "DEBUG:            idPath='#{idPath}'"
         elsif File.file?(abspath)
-            abspath = File.dirname(abspath)
             puts "DEBUG: listing parent dir of '#{abspath}'"
-        else
-            abspath = File.expand_path('~')
-            puts "DEBUG: dir '#{abspath}' does not exist! default = '#{abspath}'"
-        end
+            abspath = File.dirname(abspath)
+	end
          
         dirs = Array.new()
         files = Array.new()
@@ -68,8 +107,8 @@ module FileSystemProxy
                      'path' => p,
                      'ext'  => 'ext_foo',
                      'access' => FileTest.readable?(p)}
-            if entry['type'} == 'file'
-                entry['access'] = FileTest.writeable?(p)
+            if entry['type'] == 'file'
+                entry['access'] = FileTest.writable?(p)
             end
             if entry['type'] == 'file' && p.length > 3
                 entry['ext'] = "ext_%s" % p.slice(-3,3)
