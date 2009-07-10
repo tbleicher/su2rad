74cffc3d4897916184335cad10be2b5dadc95fa9
tbleicher <tbleicher@ae7bd4c8-e53d-0410-bc55-1557d9083968>
2009-07-10 06:51:26 +0000

    tagging su2rad_daysim_r03 snapshot

---
+++ su2radlib/html/js/tzinfo.js
@@ -0,0 +1,60 @@
+var tzabbr = [];
+// New Zealand
+txabbr[0] = "NZDT";
+txabbr[1] = "NZST"; 
+// Australia
+txabbr[2] = "AEDT"; 
+txabbr[3] = "ACDT";
+txabbr[4] = "AEST"; 
+txabbr[5] = "ACST"; 
+txabbr[6] = "AWDT"; 
+txabbr[7] = "AWST"; 
+txabbr[8] = "CXT" ; 
+// Europe
+txabbr[9]  = "MSD";
+txabbr[10] = "MSK/EEST";
+txabbr[11] = "EET/CEST";
+txabbr[12] = "CET/BST/WEST";
+txabbr[13] = "GMT/WET";
+// North America
+txabbr[14] = "NDT";
+txabbr[15] = "ADT";
+txabbr[16] = "NST";
+txabbr[17] = "AST/EDT";
+txabbr[18] = "EST";
+txabbr[19] = "MDT";
+txabbr[20] = "MST/PDT";
+txabbr[21] = "PST/AKDT";
+txabbr[22] = "AKST";
+
+var tzdiff = [];
+// New Zealand
+txdiff[0] = 13;
+txdiff[1] = 12;
+// Australia
+tzdiff[2] = 11;
+tzdiff[3] = 10.5;
+tzdiff[4] = 10;
+tzdiff[5] = 9.5;
+tzdiff[6] = 9;
+tzdiff[7] = 8;
+tzdiff[8] = 7;
+// Europe
+tzdiff[9]  = 4;
+tzdiff[10] = 3;
+tzdiff[11] = 2;
+tzdiff[12] = 1;
+tzdiff[13] = 0;
+// North America
+tzdiff[14] = -2.5;
+tzdiff[15] = -3;
+tzdiff[16] = -3.5;
+tzdiff[17] = -4;
+tzdiff[18] = -5;
+tzdiff[19] = -6;
+tzdiff[20] = -7;
+tzdiff[21] = -8;
+tzdiff[22] = -9;
+
+function getTZInfo() {
+}
---
+++ su2radlib/scene_materials.rb
@@ -0,0 +1,151 @@
+
+require 'radiance.rb'
+require 'export_modules.rb'
+require 'exportbase.rb'
+
+class ListMaterial
+    
+    include JSONUtils
+    include RadianceUtils
+   
+    attr_reader :name, :alias
+    attr_writer :alias
+    
+    def initialize(material, group='undef')
+        @material = material
+        @name = material.name
+        @alias = ''
+        @group = group
+    end
+    
+    def getDict
+        dict = {'name'     => '%s' % @material.name,
+                'nameRad'  => '%s' % getRadianceIdentifier(@material.name),
+                'nameHTML' => '%s' % JSONUtils::escapeHTML(@material.name),
+                'alias'    => '%s' % @alias,
+                'group'    => '%s' % @group}
+        return dict
+    end
+    
+    def setAlias(newAlias)
+        @alias = newAlias
+    end
+    
+    def toJSON
+        return toStringJSON(getDict())
+    end  
+
+end
+
+
+class SkmMaterial < ListMaterial
+    
+    def getDict
+        d = super()
+        d['radName'] = getRadianceIdentifier(@material.display_name)
+        return d
+    end
+    
+end
+
+
+class RadMaterial < ListMaterial
+    
+    def getDict
+        d = super()
+        d['group']      = @material.getGroup()
+        d['defType']    = @material.defType
+        d['definition'] = @material.getText().gsub(/\n/, '<br/>')
+        d['required']   = @material.required
+        d['mType']      = @material.getType()
+        return d
+    end
+    
+end
+
+
+
+class MaterialLists < ExportBase
+    
+    ## creates SketchUp, Radiance and 'layer' materials
+    
+    include JSONUtils
+    include InterfaceBase
+
+    attr_reader :matLib
+    
+    def initialize
+        path = File.join( File.dirname(__FILE__), 'ray')
+        @matLib = Radiance::MaterialLibrary.new(path)
+    end
+        
+    def setMaterialAlias(dlg, param)
+        skmname, radname, mtype = param.split("&")
+        uimessage("setting alias for %s '%s' to material '%s'" % [mtype,skmname,radname], 0)
+        dictName = "SU2RAD_ALIAS_%s" % mtype.upcase()
+        printf "DEBUG dictName=%s\n"  % dictName
+        Sketchup.active_model.set_attribute(dictName, skmname, radname)
+    end
+    
+    def removeMaterialAlias(dlg, param)
+        skmname, mtype = param.split("&")
+        dictName = "SU2RAD_ALIAS_%s" % mtype.upcase()
+        if dictName != 'SU2RAD_ALIAS_SKM' && dictName != 'SU2RAD_ALIAS_LAYER'
+            printf "\nWARNING ALIAS dictName='#{dictName}'\n"
+        end
+        radname = Sketchup.active_model.get_attribute(dictName, skmname)
+        if radname
+            attrDict = Sketchup.active_model.attribute_dictionary(dictName)
+            oldalias = attrDict.delete_key(skmname)
+            uimessage("removed alias for skm '%s' (was: '%s')" % [skmname,oldalias], 0)
+        else
+            uimessage("no alias set for %s '%s'" % [mtype, skmname]) 
+        end
+    end
+   
+    def setLists(dlg)
+        setMaterialList(dlg, 'rad')
+        setMaterialList(dlg, 'skm')
+        setMaterialList(dlg, 'layer')
+    end
+
+    def setMaterialList(dlg, mtype)
+        if mtype == 'rad'
+            materials = @matLib.getMaterials()
+            matList = materials.collect { |mat| RadMaterial.new(mat) }
+            _setMaterialListInChunks(matList, dlg, mtype)
+        elsif mtype == 'layer'
+            layers = Sketchup.active_model.layers
+            layerList = layers.collect { |layer| ListMaterial.new(layer, 'layer') }
+            attrDict = Sketchup.active_model.attribute_dictionary("SU2RAD_ALIAS_LAYER")
+            if attrDict
+                layerList.each { |m| m.alias = attrDict[m.name] || m.alias } 
+            end
+            _setMaterialListInChunks(layerList, dlg, mtype)
+        else
+            materials = Sketchup.active_model.materials
+            skmList = materials.collect { |skm| SkmMaterial.new(skm, 'skm') }
+            attrDict = Sketchup.active_model.attribute_dictionary("SU2RAD_ALIAS_SKM")
+            if attrDict
+                skmList.each { |m| m.alias = attrDict[m.name] || m.alias } 
+            end
+            _setMaterialListInChunks(skmList, dlg, 'skm')
+        end
+    end
+
+    def _setMaterialListInChunks(mList, dlg, mtype)
+        nChunk = 200
+        startIdx = 0
+        while startIdx < mList.length
+            chunk = mList[startIdx...startIdx+nChunk]
+            jsonList = chunk.collect { |m| m.toJSON() }
+            json = encodeJSON( "[%s]" % jsonList.join(',') )
+            uimessage("mList %s: setting materials %d to %d" % [mtype, startIdx, startIdx+chunk.length])
+            dlg.execute_script("setMaterialsListJSON('%s','%s')" % [json,mtype])
+            startIdx += nChunk
+        end
+    end
+
+end 
+
+
