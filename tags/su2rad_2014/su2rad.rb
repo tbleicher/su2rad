5853f8a9c77409c4e72809619e2c217b49aef64d
tbleicher@gmail.com <tbleicher@gmail.com@ae7bd4c8-e53d-0410-bc55-1557d9083968>
2014-05-25 15:09:26 +0000

    created extension figured out use of module as namespace
    new:    su2radlib/su2rad_loader.rb

--- tags/su2rad_2014/su2rad.rb
+++ tags/su2rad_2014/su2rad.rb
@@ -2,8 +2,6 @@
 #
 # su2rad.rb 
 
-$SU2RAD_VERSION = "2014.0.0" #XXX
-
 # Written by Thomas Bleicher
 #
 # tbleicher@gmail.com
@@ -63,189 +61,25 @@ $SU2RAD_VERSION = "2014.0.0" #XXX
 #                       added exception error display to export function
 # v 0.0  - 28/10/07  :  initial release
 
+module Su2rad
 
+    COPYRIGHT = "(c) 2014 by Thomas Bleicher"
+    CREATOR = "Thomas Bleicher"
+    VERSION = "2014.0.0"
 
-if RUBY_PLATFORM =~ /darwin/
-    $SU2RAD_PLATFORM = 'MAC'
-else
-    $SU2RAD_PLATFORM = 'WIN'
 end
 
-## extend search path to subdirectories
-basedir = File.dirname(__FILE__)
-$:.push(File.join(basedir, 'su2radlib'))
-#$:.push(File.join(basedir, 'su2radlib', 'rubylib'))
 
 require "sketchup.rb"
-require "su2radlib/exportbase.rb"
-require "su2radlib/context.rb"
-require "su2radlib/numeric.rb"
-require "su2radlib/material.rb"
-require "su2radlib/radiance_entities.rb"
-require "su2radlib/radiancescene.rb"
-require "su2radlib/webdialog.rb"
-require "su2radlib/config_class.rb"
+require "extensions.rb"
 
-## define defaults if config file is messed up
-$SU2RAD_LOGLEVEL    = -1        #XXX report warnings and errors only
-$SU2RAD_LOG = []
+su2rad_extension = SketchupExtension.new('su2rad', 'su2radlib/su2rad_loader.rb')
+su2rad_extension.copyright = Su2rad::COPYRIGHT
+su2rad_extension.creator = Su2rad::CREATOR
+su2rad_extension.description = "SketchUp to Radiance exporter; " +
+    "creates new entry in \"Plugins\" menu when loaded"
+su2rad_extension.version = Su2rad::VERSION
 
-## load configuration from file
-#loadPreferences()
-
-
-def startExport(selected_only=0)
-    begin
-        $SU2RAD_CONFIG = RunTimeConfig.new()
-        $SU2RAD_COUNTER = ProgressCounter.new()
-        rs = RadianceScene.new()
-        rs.startExport(selected_only)
-    rescue => e 
-        msg = "%s\n\n%s" % [$!.message,e.backtrace.join("\n")]
-        UI.messagebox msg            
-    end 
-end
-
-
-def startWebExport(selected_only=0)
-    begin
-        if $SU2RAD_DIALOG_WINDOW
-            $SU2RAD_DIALOG_WINDOW.bring_to_front()
-        else 
-            $SU2RAD_CONFIG = RunTimeConfig.new()
-            $SU2RAD_COUNTER = ProgressCounter.new()
-            edw = ExportDialogWeb.new()
-            edw.show()
-        end
-    rescue => e 
-        msg = "%s\n\n%s" % [$!.message,e.backtrace.join("\n")]
-        UI.messagebox msg            
-    end 
-end
-
-
-
-$matConflicts = nil
-
-def countConflicts
-    $SU2RAD_CONFIG = RunTimeConfig.new()
-    if $matConflicts == nil
-        $matConflicts = MaterialConflicts.new()
-    end
-    $matConflicts.count()
-end
-
-def resolveConflicts
-    $SU2RAD_CONFIG = RunTimeConfig.new()
-    if $matConflicts == nil
-        $matConflicts = MaterialConflicts.new()
-    end
-    $matConflicts.resolve()
-end
-
-def startImport()
-    $SU2RAD_CONFIG = RunTimeConfig.new()
-    ni = NumericImport.new()
-    if $SU2RAD_DEBUG
-        if ni.loadFile('/Users/ble/tmp/numimport/ADF_medium.df') == true
-            ni.confirmDialog
-            #ni.createMesh
-            #ni.addContourLines
-            #ni.addLabels
-        end
-    else
-        if ni.loadFile() == true
-            ni.confirmDialog
-        end
-    end
-end
-
-def aboutDialog
-    msg = "su2rad.rb\nSketchUp to Radiance exporter\nversion:  #{$SU2RAD_VERSION}"
-    msg += "\n(c) Thomas Bleicher, 2008\ntbleicher@gmail.com"
-    UI.messagebox(msg, MB_OK, 'su2rad.rb')
-end
-
-def acknowledgementDialog
-    dlg = UI::WebDialog.new("su2rad - acknowledgement", true, nil, 650, 800, 50, 50, true);
-    html = File.join(File.dirname(__FILE__), "su2radlib", "html","acknowledgement.html")
-    dlg.set_file(html, nil)
-    dlg.show()
-end
-    
-def preferencesDialog
-    pd = PreferencesDialog.new()
-    pd.showDialog()
-end
-
-def runTest
-    $SU2RAD_CONFIG = RunTimeConfig.new()
-    sky = RadianceSky.new()
-    sky.test()
-end
-
-def su2rad_reload
-    ## reload all script files for debugging
-    printf "reloading modules ...\n"
-    load "su2radlib/export_modules.rb"
-    load "su2radlib/exportbase.rb"
-    load "su2radlib/filesystemproxy.rb"
-    load "su2radlib/context.rb"
-    load "su2radlib/numeric.rb"
-    load "su2radlib/material.rb"
-    load "su2radlib/radiance.rb"
-    load "su2radlib/radiance_entities.rb"
-    load "su2radlib/radiancescene.rb"
-    load "su2radlib/webdialog.rb"
-    load "su2radlib/webdialog_options.rb"
-    load "su2radlib/webdialog_views.rb"
-    load "su2radlib/scene_materials.rb"
-    load "su2radlib/config_class.rb"
-    # set debug flag and reload main file to start dialog
-    $SU2RAD_DEBUG = true
-    load "su2rad.rb"
-end
-
-
-def addRadianceMenu
-    pmenu = UI.menu("Plugin")
-    radmenu = pmenu.add_submenu("Radiance")
-    radmenu.add_item("export (#{$SU2RAD_VERSION})") { startWebExport(0) }
-    radmenu.add_separator()
-    #radmenu.add_item("export scene") { startExport(0) }
-    #radmenu.add_item("export selection") { startExport(1) }
-    
-    #matmenu = radmenu.add_submenu("Material")
-    #matmenu.add_item("count conflicts") { countConflicts }
-    #matmenu.add_item("resolve conflicts") { resolveConflicts }
-    
-    importmenu = radmenu.add_submenu("Import")
-    importmenu.add_item("rtrace values") { startImport() }
-    radmenu.add_separator()
-    
-    #radmenu.add_item("Preferences") { preferencesDialog() }
-    radmenu.add_item("About") { aboutDialog() }
-    radmenu.add_item("Acknowledgement") { acknowledgementDialog() }
-    radmenu.add_separator()
-    radmenu.add_item("reload") { su2rad_reload() }
-end
-
-
-if $SU2RAD_DEBUG
-    printf "debug mode\n"
-    startWebExport(0)
-else
-    ## create menu entry
-    begin
-        if (not file_loaded?("su2rad.rb"))
-            addRadianceMenu()
-        end
-    rescue => e
-        msg = "%s\n\n%s" % [$!.message,e.backtrace.join("\n")]
-        UI.messagebox msg
-        printf "su2rad: entry to menu 'Plugin' failed:\n\n%s\n" % msg
-    end
-    file_loaded("su2rad.rb")
-end
+Sketchup.register_extension(su2rad_extension, true)
 
 
