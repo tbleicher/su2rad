9cef67cdddd23c3f6ce04a3a9e194bd5aa318f23
tbleicher@gmail.com <tbleicher@gmail.com@ae7bd4c8-e53d-0410-bc55-1557d9083968>
2014-06-25 02:21:14 +0000

    first rspec tests

--- su2rad/specs/su2rad_loader_spec.rb
+++ su2rad/specs/su2rad_loader_spec.rb
@@ -14,4 +14,45 @@ describe Su2rad do
         expect(Su2rad::CREATOR)
         expect(Su2rad::COPYRIGHT)
     end
+
+    describe "::addRadianceMenu" do
+        before(:each) do
+            @subsub = double("subsub")
+            allow(@subsub).to receive(:add_item)
+            allow(@subsub).to receive(:add_separator)
+            allow(@subsub).to receive(:add_submenu)
+            @submenu = double("submenu")
+            allow(@submenu).to receive(:add_separator)
+            allow(@submenu).to receive(:add_submenu).and_return(@subsub)
+            menu = double("menu")
+            allow(menu).to receive(:add_submenu).and_return(@submenu)
+            expect(UI).to receive(:menu) { menu }
+        end
+        context "with debug not set" do
+            before do
+                Su2rad.debug = false
+            end
+            it "creates basic menu entries" do
+                expect(@submenu).to receive(:add_item).with("export").once
+                expect(@submenu).to receive(:add_item).with(/About/).once
+                expect(@submenu).to receive(:add_item).with("Acknowledgement").once
+                expect(@submenu).to receive(:add_separator).twice
+                expect(@submenu).to receive(:add_submenu).with("Import").once
+                Su2rad::addRadianceMenu()
+            end
+        end
+        context "with debug set" do
+            before do
+                Su2rad.debug = true
+            end
+            it "creates basic and debug menu entries" do
+                expect(@submenu).to receive(:add_item).exactly(4).times
+                expect(@submenu).to receive(:add_separator).exactly(3).times
+                Su2rad::addRadianceMenu()
+            end
+        end
+    end
+
+    
+
 end
--- su2rad/su2rad_module.rb
+++ su2rad/su2rad_module.rb
@@ -3,6 +3,15 @@ module Su2rad
     COPYRIGHT = "(c) 2014 by Thomas Bleicher"
     CREATOR = "Thomas Bleicher"
     VERSION = "2014.0.0"
+    
+    @@debug = false
 
+    def self.debug
+        @@debug
+    end
+
+    def self.debug=(d)
+        @@debug = d
+    end
 end
 
