80237919dde9eca87dc4532e2c22ac3bbf683d73
tbleicher <tbleicher@gmail.com>
2016-05-04 21:14:29 -0500

    hiding output of printf in tests

--- su2rad/spec/modules/logger_spec.rb
+++ su2rad/spec/modules/logger_spec.rb
@@ -1,5 +1,16 @@
 require_relative '../../su2radlib/modules/logger.rb'
 
+require 'stringio'
+
+def capture_stdout(&blk)
+  old = $stdout
+  $stdout = fake = StringIO.new
+  blk.call
+  fake.string
+ensure
+  $stdout = old
+end
+
 class LogUser
   include Su2Rad::Logger
 end
@@ -28,11 +39,13 @@ describe Su2Rad::Logger do
 
   it "is shared among all users" do
     allow(@sketchup).to receive(:set_status_text)
-    expect(Su2Rad::Logger.output.length).to be(0)
-    @foo.uimessage('message from a', 0, @sketchup)
-    expect(Su2Rad::Logger.output.length).to be(1)
     b = LogUserB.new()
-    b.uimessage('message from b', 0, @sketchup)
+    expect(Su2Rad::Logger.output.length).to be(0)
+    printed = capture_stdout do
+      @foo.uimessage('message from a', 0, @sketchup)
+      #expect(Su2Rad::Logger.ou  tput.length).to be(1)
+      b.uimessage('message from b', 0, @sketchup)
+    end
     expect(Su2Rad::Logger.output.length).to be(2)
   end
 
@@ -41,7 +54,9 @@ describe Su2Rad::Logger do
 
     it "resets Su2Rad::Logger::LOG" do
       allow(@sketchup).to receive(:set_status_text)
-      @foo.uimessage("a message", 0, @sketchup)
+      printed = capture_stdout do
+        @foo.uimessage("a message", 0, @sketchup)
+      end
       expect(Su2Rad::Logger.output.length).to be(1)
       Su2Rad::Logger.initLog()
       expect(Su2Rad::Logger.output.length).to be(0)
@@ -61,37 +76,49 @@ describe Su2Rad::Logger do
 
     it "adds info indicator to messages" do
       allow(@sketchup).to receive(:set_status_text).with("[I] log message")
-      @foo.uimessage('log message', 0, @sketchup)
+      printed = capture_stdout do
+        @foo.uimessage('log message', 0, @sketchup)
+      end
     end
 
     it "adds warning indicator to messages" do
       allow(@sketchup).to receive(:set_status_text).with("[W] log message")
-      @foo.uimessage('log message', -1, @sketchup)
+      printed = capture_stdout do
+        @foo.uimessage('log message', -1, @sketchup)
+      end
     end
 
     it "adds error indicator to messages" do
       allow(@sketchup).to receive(:set_status_text).with("[E] log message")
-      @foo.uimessage('log message', -2, @sketchup)
+      printed = capture_stdout do
+        @foo.uimessage('log message', -2, @sketchup)
+      end
     end
 
     it "adds warning messages to the counter" do
       allow(@sketchup).to receive(:set_status_text).with("[W] log message")
       counter = double("Su2Rad::Counter")
       allow(counter).to receive(:add).with("warnings")
-      @foo.uimessage('log message', -1, @sketchup, counter)
+      printed = capture_stdout do
+        @foo.uimessage('log message', -1, @sketchup, counter)
+      end
     end
 
     it "adds error messages to the counter" do
       allow(@sketchup).to receive(:set_status_text).with("[E] log message")
       counter = double("Su2Rad::Counter")
       allow(counter).to receive(:add).with("errors")
-      @foo.uimessage('log message', -2, @sketchup, counter)
+      printed = capture_stdout do
+        @foo.uimessage('log message', -2, @sketchup, counter)
+      end
     end
 
     it "adds messages to Su2Rad::Logger::LOG" do
       allow(@sketchup).to receive(:set_status_text)
       expect(Su2Rad::Logger.output.length).to be(0)
-      @foo.uimessage('log message', 0, @sketchup)
+      printed = capture_stdout do
+        @foo.uimessage('log message', 0, @sketchup)
+      end
       expect(Su2Rad::Logger.output.length).to be(1)
     end
 
@@ -101,7 +128,9 @@ describe Su2Rad::Logger do
   describe '#closeLog' do
     it "adds final lines to output" do
       allow(@sketchup).to receive(:set_status_text)
-      @foo.uimessage('log message', 0, @sketchup)
+      printed = capture_stdout do
+        @foo.uimessage('log message', 0, @sketchup)
+      end
       expect(Su2Rad::Logger.output.length).to be(1)
       Su2Rad::Logger.closeLog()
       expect(Su2Rad::Logger.output.length).to be(3)
@@ -114,13 +143,13 @@ describe Su2Rad::Logger do
 
   describe '#write' do
     it "writes a log file" do
-
       path = "/path/to/logfile.log"
       io = "" 
       allow(File).to receive(:open).with(path, "a").and_return(io)
       allow(@sketchup).to receive(:set_status_text)
-
-      @foo.uimessage('final message', 0, @sketchup)
+      printed = capture_stdout do
+        @foo.uimessage('final message', 0, @sketchup)
+      end
       expect(Su2Rad::Logger.output.length).to be(1)
       Su2Rad::Logger.write("/path/to/logfile.log")
       expect( io ).to eq("[I] final message")
@@ -131,8 +160,9 @@ describe Su2Rad::Logger do
       allow(File).to receive(:open).with(path, "a").and_raise("boom")
       allow(@sketchup).to receive(:set_status_text).with(/failed/)
       allow(@sketchup).to receive(:set_status_text).with(/logfile\.log/)
-
-      Su2Rad::Logger.write(path, @sketchup)
+      printed = capture_stdout do  
+        Su2Rad::Logger.write(path, @sketchup)
+      end
     end
   
   end # #writeLogFile
